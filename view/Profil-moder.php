72d4cc259393ce239f6ccc36a5bb33c24b32c4bc
amzafard <84776744+amzafard@users.noreply.github.com>
2021-06-01 18:12:11 +0200

    Update Profil-moder.php

--- view/Profil-moder.php
+++ view/Profil-moder.php
@@ -16,7 +16,7 @@ include "header.php";
 						<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> -->
 						<link href="../ressources/css/styles-porifl.css" rel="stylesheet">
 						</head>
-                        <title> Profil</title>
+                        <title> Profil Moderateur</title>
 						<body>
                         
 							<div class="row dashboard">
